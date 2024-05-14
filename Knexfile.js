04eb60fbff48cd66cfea99eab75412e31e9ad500
sarra2108 <ssass058@uottawa.ca>
2024-05-13 22:21:51 -0400

    back-end

---
+++ Knexfile.js
@@ -0,0 +1,15 @@
+// Knexfile.js
+module.exports = {
+    development: {
+      client: 'pg',
+      connection: {
+        host: '127.0.0.1',
+        user: 'postgres',
+        password: '210803',
+        database: 'smart-brain-api',
+        port: '5432', // Optional, default PostgreSQL port is 5432
+      },
+      
+    },
+  };
+  
